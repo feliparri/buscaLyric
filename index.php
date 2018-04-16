a03ff3c73bf95677546aa4132b2bce5c623556e4
felipe huerta iturra <felipe.huerta.iturra@gmail.com>
2018-04-16 15:46:32 -0400

    inicio de desarrollo de buscador de temas y creador de listas buscaLyric

---
+++ index.php
@@ -0,0 +1,10 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+<section class="webdesigntuts-workshop">
+	<form action="" method="">		    
+		<input type="search" placeholder="Buscar letras, listas, audios, etc.">		    	
+		<button>Buscar</button>
+	</form>
+</section>
